ba243a55a5cd641017ee118b15fc9b678fedac86
Chinmai Shetty <66892324+chinmaishetty262@users.noreply.github.com>
2026-06-07 12:53:03 +0200

    build fix

--- authservice/src/main/java/com/authservice/auth/config/MetricsConfig.java
+++ authservice/src/main/java/com/authservice/auth/config/MetricsConfig.java
@@ -1,15 +1,10 @@
 package com.authservice.auth.config;
 
-import io.micrometer.core.instrument.MeterRegistry;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+
+
 
 @Configuration
 public class MetricsConfig {
-
-    @Bean
-    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-        return registry -> registry.config().commonTags("application", "authservice");
-    }
-}
+} 
