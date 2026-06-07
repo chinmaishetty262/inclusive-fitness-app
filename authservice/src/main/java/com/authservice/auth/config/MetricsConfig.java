f1bd141c7f1695a523be841ecf2d754484dae062
Chinmai Shetty <66892324+chinmaishetty262@users.noreply.github.com>
2026-06-07 11:32:43 +0200

    added-metrics-config-file

---
+++ authservice/src/main/java/com/authservice/auth/config/MetricsConfig.java
@@ -0,0 +1,8 @@
+package com.authservice.auth.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+}
